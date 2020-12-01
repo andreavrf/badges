a047f64c77d0591825ce9527b9bd5e5542f6f5ba
andrea <andreavanessaros@gmail.com>
2020-12-01 22:19:58 +0100

    editar elemento

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
-
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  FormGroup,
+} from "reactstrap";
 import App from "./components/App";
 const container = document.getElementById("app");
 
